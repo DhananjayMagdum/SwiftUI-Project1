197251924be55de0467b624e5cda5ba860a272fc
Dhananjay Magdum <dhananjay.magdum@atlascopco.com>
2021-02-27 06:55:35 +0530

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Dhananjay Magdum on 27/02/21.
+//  Copyright © 2021 Atlas Copco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
